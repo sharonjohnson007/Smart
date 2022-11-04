29d75f54f2bce87410e87a394ecdb078afcc5cd0
Sharon Johnson <sharonjohnson.manuvel@gcitsloutions.com>
2022-11-04 17:12:42 +0530

    commit

---
+++ src/Mobile.java
@@ -0,0 +1,32 @@
+public class Mobile {
+    String mobilename;
+    String colour;
+    float length;
+    float breath;
+
+void display()
+{
+    System.out.println("Shows the information in image,video or text format ");
+}
+void camera()
+{
+    System.out.println("captures the targeted image");
+}
+void speaker()
+{
+    System.out.println("produce sounds");
+}
+void processor()
+{
+  System.out.println("process the users task");
+}
+void battery()
+{
+    System.out.println("Stores and supply electric power");
+}
+public static void main(String args[])
+{
+    Mobile m=new Mobile();
+    m.camera();
+}
+}
