28a032a7ae0aa36bca8f335fc9042113093b7d90
Sharon Johnson <sharonjohnson.manuvel@gcitsloutions.com>
2022-12-07 13:49:55 +0530

    changes after review
    changes after review

--- Day20/src/main/java/IncomeTaxCalculator.java
+++ Day20/src/main/java/IncomeTaxCalculator.java
@@ -10,35 +10,46 @@ public class IncomeTaxCalculator {
     final double TAX_RATE_ABOVE_20K = 0.1;
     final double TAX_RATE_ABOVE_40K = 0.2;
     final double TAX_RATE_ABOVE_60K = 0.3;
-    final double TAX_REBATE_ABOVE_1K = 1000;
-
+    
     public static void main(String[] args) {
         IncomeTaxCalculator incomeTaxCalculator = new IncomeTaxCalculator();
         int taxableIncome;
         double taxPayable = 0;
+        double amount;
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter the taxable income: ");
         taxableIncome = scanner.nextInt();
         if (taxableIncome <= 20000) {          //For below 20000 taxPayable=0.00
             taxPayable = 0;
         } else if (taxableIncome <= 40000) {   //For below 40000 taxPayable=0.1%
-            taxPayable = taxableIncome * incomeTaxCalculator.TAX_RATE_ABOVE_20K;
+            amount = taxableIncome - 20000;
+            taxPayable = amount * incomeTaxCalculator.TAX_RATE_ABOVE_20K;
         } else if (taxableIncome <= 60000) {   //For below 60000 taxPayable=0.2%
-            taxPayable = taxableIncome * incomeTaxCalculator.TAX_RATE_ABOVE_40K;
+            amount = taxableIncome - 20000;
+            if (amount > 20000) {
+                amount = amount - 20000;
+                taxPayable = 20000 * incomeTaxCalculator.TAX_RATE_ABOVE_20K + amount * incomeTaxCalculator.TAX_RATE_ABOVE_40K;
+            } else {
+                taxPayable = amount * incomeTaxCalculator.TAX_RATE_ABOVE_20K;
+            }
         } else {                               //For above 60000 taxPayable=0.3%
-            taxPayable = taxableIncome * incomeTaxCalculator.TAX_RATE_ABOVE_60K;
+            amount = taxableIncome - 20000;
+            if (amount > 40000) {
+                amount = amount - 20000;
+                amount = amount - 20000;
+                taxPayable = 20000 * 0.0 + 20000 * incomeTaxCalculator.TAX_RATE_ABOVE_20K + 20000 * incomeTaxCalculator.TAX_RATE_ABOVE_40K + amount * incomeTaxCalculator.TAX_RATE_ABOVE_60K;
+            } else {
+                taxPayable = 20000 * incomeTaxCalculator.TAX_RATE_ABOVE_20K + amount * incomeTaxCalculator.TAX_RATE_ABOVE_40K;
+            }
         }
         System.out.printf("The income tax payable is: $%.2f%n", taxPayable);
-
         //For 10% tax rebate is announced for the income tax payable, capped at $1,000
-        Scanner scanner1 = new Scanner(System.in);
-        System.out.print("Enter the taxable income: ");
-        taxableIncome = scanner1.nextInt();
-        if (taxableIncome <= 20000) {           //For below 20000 taxPayable=0.00
-            taxPayable = 0.00;
-        } else {                               // If taxPayable exceeds 1000 by rebate taxPayable=1000
-            taxPayable = incomeTaxCalculator.TAX_REBATE_ABOVE_1K;
-            System.out.printf("The income tax payable is: $%.2f%n", taxPayable);
+        if (taxPayable <= 1000) {
+            double rebate = taxPayable * 0.1;
+            taxPayable = taxPayable - rebate;
+        } else {
+            taxPayable = taxPayable - 1000;
         }
+        System.out.println("TaxPayable after rebate: " + taxPayable);
     }
 }
