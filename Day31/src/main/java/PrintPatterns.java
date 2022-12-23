c1a688a449fecd03c2933dcd6d94f735a075b19c
Sharon Johnson <sharonjohnson.manuvel@gcitsloutions.com>
2022-12-23 15:47:21 +0530

    changes after review

--- Day31/src/main/java/PrintPatterns.java
+++ Day31/src/main/java/PrintPatterns.java
@@ -16,6 +16,7 @@ public class PrintPatterns {
         printPatternI(8);
         printPatternJ(8);
         printPatternK(8);
+        printPatternL(8);
     }
 
     public static void printPatternA(int size) {
@@ -174,24 +175,23 @@ public class PrintPatterns {
 
     public static void printPatternJ(int size) {
         for (int row = 1; row <= size; row++) {
-            for (int col = 1; col <= size; col++) {
-                if (row >= col) {
-                    System.out.print(col + " ");
-                }
-            }
-            for (int space = size - 1; space >= row; space--) {
-                System.out.print("    ");
-            }
-            for (int col = row; col >= 1; col--) {
-                if (col == 8) {
+            for (int col = 1; col <= 2 * size; col++) {
+                if (col % (size + 1) == 0) {
                     continue;
+                } else if ((col <= size) && (col > row)) {
+                    System.out.print(" ");
+                } else if ((col > size + 1) && (col < 2 * size - row + 1)) {
+                    System.out.print(" ");
+                } else if (col <= size) {
+                    System.out.print(col % (size + 1));
+                } else {
+                    System.out.print(2 * size - col + 1);
                 }
-                System.out.print(col + " ");
+                System.out.print(" ");
             }
             System.out.println();
         }
         System.out.println("(j)------------------");
-
     }
 
     public static void printPatternK(int size) {
@@ -199,7 +199,6 @@ public class PrintPatterns {
             for (int j = 1; j <= i; j++) {
                 System.out.print(j + " ");
             }
-
             for (int j = i * 2; j < size * 2 - 1; j++) {
                 System.out.print("  ");
             }
@@ -211,7 +210,43 @@ public class PrintPatterns {
         }
         System.out.println("(k)------------------");
     }
+
+    public static void printPatternL(int size) {
+        int k = 0, count = 0, count1 = 0;
+        for (int i = 1; i <= size; ++i) {
+            for (int space = 1; space <= size - i; ++space) {
+                System.out.print("  ");
+                ++count;
+            }
+            while (k != 2 * i - 1) {
+                if (count <= size - 1) {
+                    int value = (i + k);
+                    if (value > 9) {
+                        value = value % 10;
+                    }
+                    System.out.print(value + " ");
+                    ++count;
+                } else {
+                    ++count1;
+                    int values = (i + k - 2 * count1);
+                    if (values > 9) {
+                        values = values % 10;
+                    }
+                    System.out.print(values + " ");
+                }
+                ++k;
+            }
+            count1 = count = k = 0;
+            System.out.println();
+        }
+        System.out.println("(l)------------------");
+    }
 }
+
+
+
+
+
 
 
 
